b7eab753298c3bf10b7ace30f7e0e566dd292a87
as39248 <andyshim02@gmail.com>
2025-09-08 21:27:20 -0700

    bug on saved ticker page fixed

--- app/ui/saved_ticker.py
+++ app/ui/saved_ticker.py
@@ -11,14 +11,16 @@ def saved_ticker_page():
     current_user = st.session_state.username
 
     saved_tickers = view_saved_ticker(current_user)
-
-    for i in range(len(saved_tickers)):
-        st.subheader(saved_tickers[i])
-        if st.button("Search", key=f"search_{i}"):
-            search_result(saved_tickers[i])
-        if st.button("Delete", key=f"delete_{i}"):
-            delete_ticker(saved_tickers[i])
-            st.success(f"{saved_tickers[i]} deleted")
-            st.rerun() 
+    if not saved_ticker_page:
+        for i in range(len(saved_tickers)):
+            st.subheader(saved_tickers[i])
+            if st.button("Search", key=f"search_{i}"):
+                search_result(saved_tickers[i])
+            if st.button("Delete", key=f"delete_{i}"):
+                delete_ticker(saved_tickers[i])
+                st.success(f"{saved_tickers[i]} deleted")
+                st.rerun() 
+    else:
+        st.subheader("No Saved Tickers")
         
     
